c0e7bc574478a6d18476ca3d0fceb6bd3e2c6102
pedroAntonio <pedro.salinas25@aol.com>
2023-08-24 17:30:41 -0500

    weather search by city

--- PFappClima/src/App.jsx
+++ PFappClima/src/App.jsx
@@ -3,7 +3,6 @@ import './App.css'
 import HistoryWheathe from './components/HistoryWheathe'
 import PlaceWheather from './components/PlaceWheather'
 
-console.log (import.meta.env.VITE_APIKEY);
 function App() {
   
   return (
